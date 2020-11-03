7d86c95b1d9acf51b0ae8bf5b028d92d22fbfbf4
Norbert Melzer <timmelzer@gmail.com>
2020-11-03 07:42:14 +0100

    start tool

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,26 @@ use std::collections::HashMap;
 use rocket::{get, routes};
 use rocket_contrib::templates::Template;
 
+use tstool::database::Tool;
+
 #[get("/")]
 fn index() -> Template {
     Template::render("index", &HashMap::<String, ()>::new())
 }
 
+#[get("/tool")]
+fn tool() -> Template {
+    Template::render(
+        "tool/show",
+        Tool {
+            name: "fnoo".into(),
+        },
+    )
+}
+
 fn main() {
     rocket::ignite()
         .attach(Template::fairing())
-        .mount("/", routes![index])
+        .mount("/", routes![index, tool])
         .launch();
 }
